f0acac4a7695e4abddbe32c1f2f4aaeca3a3a89b
Jeffrey Camealy <jeffrey@orainteractive.com>
2014-05-12 22:06:44 -0500

    make half arm head work.

--- Objective-SCAD-Objects/Objects/BikeMirror/BikeMirror.m
+++ Objective-SCAD-Objects/Objects/BikeMirror/BikeMirror.m
@@ -18,25 +18,43 @@ const float ARM_HEIGHT = 4.0;
 const float BALL_JOINT_DIAMETER = 9.5;
 
 // Arm Head
+const float SOCKET_BALL_UNDERSIZED_OFFSET_FACTOR = 0.1;
 const float SOCKET_WALL_THICKNESS = 2.0;
 const float SOCKET_DIAMETER = BALL_JOINT_DIAMETER+SOCKET_WALL_THICKNESS;
-const float SOCKET_HEIGHT = SOCKET_DIAMETER;
+const float SOCKET_HEIGHT = BALL_JOINT_DIAMETER*(1-SOCKET_BALL_UNDERSIZED_OFFSET_FACTOR)/2.0+SOCKET_WALL_THICKNESS;
+
 
 @implementation BikeMirror
 
+#pragma mark - Override
+
 - (void)buildSubObjects {
-    OSCube *arm = [[OSCube alloc] initWithSizeVector:v(ARM_LENGTH, ARM_WIDTH, ARM_HEIGHT)];
-    [arm addTransformation:translate(0, 0, -ARM_HEIGHT/2)];
+    OSCompositeObject *halfArmHead = [self halfArmHead];
+//    halfArmHead addTransformation:mir
+    [self.subObjects addObject:halfArmHead];
+}
+
+
+#pragma mark - Private API
+
+- (OSCompositeObject *)halfArmHead {
+    OSCube *arm = [[OSCube alloc] initWithSizeVector:v(ARM_LENGTH/2, ARM_WIDTH, ARM_HEIGHT)];
+    [arm addTransformation:translate(ARM_LENGTH/4, 0, -ARM_HEIGHT/2)];
     
-    OSCylinder *socket = [[OSCylinder alloc] initWithDiameter:SOCKET_DIAMETER height:SOCKET_HEIGHT];
-    [socket addTransformation:translate(ARM_LENGTH/2, 0, -SOCKET_HEIGHT/2)];
+    OSCylinder *socketBlank = [[OSCylinder alloc] initWithDiameter:SOCKET_DIAMETER height:SOCKET_HEIGHT];
+    [socketBlank addTransformation:translate(ARM_LENGTH/2, 0, -SOCKET_HEIGHT/2)];
+    
+    OSCompositeObject *armHeadBlank = [[OSCompositeObject alloc] initWithSubObjects:@[arm, socketBlank]];
+    armHeadBlank.compositeType = OSCTUnion;
     
     OSSphere *ball = [[OSSphere alloc] initWithDiameter:BALL_JOINT_DIAMETER];
     [ball addTransformation:[OSColorTransformation transformationWithColor:color(1, 0, 0, 1)]];
+    [ball addTransformation:translate(ARM_LENGTH/2, 0, BALL_JOINT_DIAMETER*SOCKET_BALL_UNDERSIZED_OFFSET_FACTOR)];
     
-    [self.subObjects addObject:arm];
-    [self.subObjects addObject:socket];
-    [self.subObjects addObject:ball];
+    OSCompositeObject *armHead = [[OSCompositeObject alloc] initWithSubObjects:@[armHeadBlank, ball]];
+    armHead.compositeType = OSCTDifference;
+    
+    return armHead;
 }
 
 @end
