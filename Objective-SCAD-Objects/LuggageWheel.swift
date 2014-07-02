3fe903b92da787d0709852a1932350a478b530d4
Jeffrey Camealy <jeffrey@Jeffreys-Mac-mini.local>
2014-07-02 02:19:03 -0500

    looks like a wheel

--- Objective-SCAD-Objects/LuggageWheel.swift
+++ Objective-SCAD-Objects/LuggageWheel.swift
@@ -8,27 +8,55 @@
 
 import Foundation
 
+let Thickness: Float = 20
+let CurveRadius: Float = 5
+let CurveResolution: CInt = 20
+let TotalDiameter: Float = 60
+let HoleDiameter: Float = 10
+let WheelResolution: CInt = 40
+
 class LuggageWheel : OSCompositeObject {
     
     override func buildSubObjects() {
         let wheelShape = makeWheelShape()
-        wheelShape.transformations.addObject(rotate(0, 0, 3.14159265358979323846264/2))
-        wheelShape.transformations.addObject(translate(10, 0, 0))
         
-        let rotationalExtrusion = OSRotationalExtrusion(with2DShape: wheelShape, resolution: 10)
+        let rotationalExtrusion = OSRotationalExtrusion(with2DShape: wheelShape, resolution: WheelResolution)
         self.subObjects.addObject(rotationalExtrusion)
     }
     
     func makeWheelShape() -> OSObject {
-        let circleRight = OSCircle(radius: 3, resolution: 10)
-        let circleLeft = OSCircle(radius: 3, resolution: 10)
+        let circleRight = OSCircle(radius: CurveRadius, resolution: CurveResolution)
+        let circleLeft = OSCircle(radius: CurveRadius, resolution: CurveResolution)
         
-        circleRight.transformations.addObject(translate(4, 0, 0))
-        circleLeft.transformations.addObject(translate(-4, 0, 0))
+        circleRight.transformations.addObject(translate(Thickness/2-CurveRadius, 0, 0))
+        circleLeft.transformations.addObject(translate(-(Thickness/2-CurveRadius), 0, 0))
         
         let wheelShape = OSCompositeObject(subObjects: [circleRight, circleLeft])
         wheelShape.compositeType = OSCTHull
+        wheelShape.transformations.addObject(translate(0, -(TotalDiameter/2-HoleDiameter/2), 0))
         
-        return wheelShape
+        let height = TotalDiameter/2-HoleDiameter/2-CurveRadius
+        let rect = OSRectangle(size: sz(Thickness, height))
+        rect.transformations.addObject(translate(0, -(height/2+HoleDiameter/2), 0))
+        
+        let moreWheel = OSCompositeObject(subObjects: [wheelShape, rect]);
+        moreWheel.transformations.addObject(rotate(0, 0, 3.14159265358979323846264/2))
+        
+        return moreWheel
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
